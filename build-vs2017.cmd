e3a51116715f2ee753c4574db6192e17b177ffb4
gyliu <gyliu@sonicom.com.cn>
2022-02-08 13:58:17 +0800

    整理Cmake

--- build-vs2017.cmd
+++ build-vs2017.cmd
@@ -3,6 +3,9 @@ del vs2017\CMakeCache.txt
 
 cmake 	-B vs2017 ^
 		-G "Visual Studio 15 2017" ^
+		-DYATE_BUILD_ALL=ON ^
+		-DYATE_BUILD_SIP=OFF ^
+		-DYATE_BUILD_TEST=ON ^
 		-DCMAKE_INSTALL_PREFIX=/install/windows/vs2017/yate ^
 		.
 
