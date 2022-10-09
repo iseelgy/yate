220c5d93a1ee3c8c10d2c7c00471bcd3b54079af
gyliu <gyliu@sonicom.com.cn>
2022-10-10 06:37:45 +0800

    调整vs2017的安装位置

--- 2.vs2017-build.cmd
+++ 2.vs2017-build.cmd
@@ -3,8 +3,8 @@ del msvs2017\CMakeCache.txt
 
 cmake 	-B msvs2017 ^
 		-G "Visual Studio 15 2017" ^
-		-DCMAKE_PREFIX_PATH=/install/windows/vs2017 ^
- 		-DCMAKE_INSTALL_PREFIX=/install/windows/vs2017/yate ^
+		-DCMAKE_PREFIX_PATH=/install/vs2017 ^
+ 		-DCMAKE_INSTALL_PREFIX=/install/vs2017/yate ^
 		.
 
 
